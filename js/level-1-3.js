b114441102a1a374c017c9f312088383045d0b21
Derek-Cruz <Derekcruz94@gmail.com>
2022-04-29 15:47:41 -0700

    redoing problem.

--- js/level-1-3.js
+++ js/level-1-3.js
@@ -1,15 +1,3 @@
 // https://app.codesignal.com/arcade/intro/level-1/s5PbmwxfECC52PWyQ
 
-// function solution(inputString) {
-//   let all = /[A-Za-z0-9]/g;
-//   inputString = inputString.toLowerCase().replace(all, '');
-//   let newStr = inputString.length;
-
-//   for (let i = 0; i < newStr/2; i++) {
-//     if (inputString[i] !== inputString[newStr - 1 - i]) {
-//         return false;
-//     }
-//   }
-//   return true
-// // }
-//  trying something else real fast
+/
