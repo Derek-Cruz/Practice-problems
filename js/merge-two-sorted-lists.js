4353ef9dbc9088d4bd1e9d944f85684b5c115d7f
Derek-Cruz <Derekcruz94@gmail.com>
2022-05-16 15:54:14 -0700

    finished problem.

--- js/merge-two-sorted-lists.js
+++ js/merge-two-sorted-lists.js
@@ -12,24 +12,20 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
+
 var mergeTwoLists = function (list1, list2) {
-
-};
-
-
-var mergeTwoLists = function (l1, l2) {
   // If either list is empty, return the other list's node
-  if (l1 == null) return l2;
-  if (l2 == null) return l1;
+  if (list1 == null) return list2;
+  if (list2 == null) return list1;
 
-  // If l1 is lower
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
+  // If list1 is lower
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
   }
-  // If l2 is lower
+  // If list2 is lower
   else {
-    l2.next = mergeTwoLists(l1, l2.next);
-    return l2;
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
   }
 };
