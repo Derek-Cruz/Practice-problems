b4f873dbfdc181eaac93be6427e1b4d218a585a9
Derek Cruz <derekcruz94@gmail.com>
2022-09-08 16:41:56 +0000

    gs question.

--- js/others/gs.js
+++ js/others/gs.js
@@ -5,3 +5,19 @@ function Oddnumbers(l, r) {
 function Sortarr(arr, k) {
 
 }
+
+function g(s) {
+    let curr = 1;
+    let prev = 0;
+    let ans = 0;
+    for (let i = 1; i < s.lenght; i++) {
+        if(s[i] === s[i-1]) {
+            curr++
+        } else {
+            ans += Math.min(curr, prev);
+            prev = curr;
+            curr = 1;
+        }
+        return ans + Math.min(curr,prev);
+    };
+}
